e04a811fbbf85254d35fc776fe80c146ab866c62
Ilan Guenet <ilan.guenet@epita.fr>
2021-01-06 20:59:01 +0100

    fix: users can pick the compute capability in the build system

--- build.py
+++ build.py
@@ -10,14 +10,18 @@ program_name = "reduce_example"
 
 class Config:
     debug = True
+    compute = 75
 
 def parse_args(config):
     parser = argparse.ArgumentParser(description='Build System')
     parser.add_argument('-d', '--debug', default=True, help='Debug', action='store_true')
     parser.add_argument('-r', '--release', default=False, help='Release', action='store_true')
+    parser.add_argument('--compute', help='GPU Compute capability')
     args = parser.parse_args()
     config.debug = args.debug
     config.debug = not args.release
+    if args.compute:
+        config.compute = args.compute
 
 def compile(config):
     if not os.path.isdir(build_dir):
@@ -26,7 +30,7 @@ def compile(config):
     cmd = ["nvcc"] +  sources
     cmd += ["--extended-lambda"] # for extended lamba __device__
     cmd += ["-o", build_dir + "/" + program_name] # output
-    cmd += ["-arch=compute_50", "-code=sm_50"]
+    cmd += ["-arch=compute_" + str(config.compute), "-code=sm_" + str(config.compute)]
     if config.debug:
         cmd += ["-g", "-G"] # debug and device-debug. Define __CUDACC_DEBUG__
     else:
