4dba7190c8e8ca07fb439bb576d130a452ae366d
Ilan Guenet <ilan.guenet@epita.fr>
2021-01-06 11:03:31 +0100

    up(main): make a reduce add test in the main file

--- main.cu
+++ main.cu
@@ -1,12 +1,68 @@
+#include <iostream>
+#include "cuda.h"
+
 #include "reduce.cuh"
 
-#include <stdio.h>
+
+/*! \brief init arrays for a reduce_add and get the result for later comparison */
+template <typename T, typename U>
+static U init_data_sum(T** d_data, U** d_result, size_t size)
+{
+    T* h_data = new T[size];
+    cudaSafeCall(cudaMalloc((void**)d_data, sizeof(T) * size));
+    cudaSafeCall(cudaMalloc((void**)d_result, sizeof(U)));
+    cudaSafeCall(cudaMemset(*d_result, 0, sizeof(U)));
+
+    U result = static_cast<U>(0);
+    for (unsigned int i = 0; i < size; ++i)
+    {
+        const uint tmp = i % 10;
+        h_data[i] = static_cast<T>(tmp);
+        result += static_cast<U>(h_data[i]);
+    }
+
+    cudaSafeCall(cudaMemcpy(*d_data, h_data, sizeof(T) * size, cudaMemcpyHostToDevice));
+
+    delete[] h_data;
+
+    return result;
+}
+
+/*! \brief Check the results of the gpu reduce and free ressources */
+template <typename T, typename U>
+static void check_result(U* d_result, U h_expected, T* d_data)
+{
+    U h_result;
+    cudaSafeCall(cudaMemcpy(&h_result, d_result, sizeof(U), cudaMemcpyDeviceToHost));
+
+    cudaSafeCall(cudaFree(d_result));
+    cudaSafeCall(cudaFree(d_data));
+
+
+    if (h_expected != h_result)
+        std::cout << "Failure. Expected: " << h_expected << " Got: " << h_result << std::endl;
+    else
+        std::cout << "Success." << std::endl;
+}
 
 
 int main()
 {
-    printf("OK\n");
-    int a = 7;
-    reduce_add(&a, &a, 1);
+    constexpr size_t size = 100000; // 100 000
+    int* d_data;
+    int* d_result;
+
+    int h_expected = init_data_sum(&d_data, &d_result, size);
+
+    // Run the reduce on default stream
+    reduce_add(d_data, d_result, size);
+
+    // Wait until the end of computation
+    // Use cudaStreamSynchronize if the reduce has been ran on a stream
+    // different than the default stream
+    cudaDeviceSynchronize();
+
+    check_result(d_result, h_expected, d_data);
+
     return 1;
 }
--- utils.cuh
+++ utils.cuh
@@ -12,4 +12,129 @@
             exit(0);                                                                      \
         }                                                                                 \
     }
+    #endif
+
+#ifndef _DEBUG
+#define cudaSafeCall(ans) ans
+#else
+#define cudaSafeCall(ans)                                                      \
+    {                                                                          \
+        gpuAssertDebug((ans), __FILE__, __LINE__);                             \
+    }
+inline void
+gpuAssertDebug(cudaError_t code, const char* file, int line, bool abort = true)
+{
+    if (code != cudaSuccess)
+    {
+        fprintf(stderr,
+                "GPUassert: %s %s %d\n",
+                cudaGetErrorString(code),
+                file,
+                line);
+        if (abort)
+            exit(code);
+    }
+}
+#endif
+
+// atomicAdd with double is not defined if CUDA Version is not greater than or
+// equal to 600 So we use this macro to keep a fully compatible program
+#if !defined(__CUDA_ARCH__) || __CUDA_ARCH__ >= 600
+#else
+__device__ double atomicAdd(double* address, double val)
+{
+    unsigned long long int* address_as_ull = (unsigned long long int*)address;
+    unsigned long long int old = *address_as_ull, assumed;
+
+    do
+    {
+        assumed = old;
+        old = atomicCAS(
+            address_as_ull,
+            assumed,
+            __double_as_longlong(val + __longlong_as_double(assumed)));
+
+        // Note: uses integer comparison to avoid hang in case of NaN (since NaN
+        // != NaN)
+    } while (assumed != old);
+
+    return __longlong_as_double(old);
+}
+#endif
+
+// atomicMin is only supported for unsigned int & long long, ushort is not
+// supported by atomicCAS (even though the doc says so)
+#if !defined(__CUDA_ARCH__)
+#else
+__device__ float atomicMin(float* address, float val)
+{
+    int* address_as_i = (int*)address;
+    int old = *address_as_i, assumed;
+    do
+    {
+        assumed = old;
+        old = atomicCAS(address_as_i,
+                        assumed,
+                        __float_as_int(fminf(val, __int_as_float(assumed))));
+    } while (assumed != old);
+    return __int_as_float(old);
+}
+__device__ double atomicMin(double* address, double val)
+{
+    unsigned long long int* address_as_ull = (unsigned long long int*)address;
+    unsigned long long int old = *address_as_ull, assumed;
+
+    do
+    {
+        assumed = old;
+        old = atomicCAS(
+            address_as_ull,
+            assumed,
+            __double_as_longlong(fmin(val, __longlong_as_double(assumed))));
+
+        // Note: uses integer comparison to avoid hang in case of NaN (since NaN
+        // != NaN)
+    } while (assumed != old);
+
+    return __longlong_as_double(old);
+}
+#endif
+
+// atomicMax is only supported for unsigned int & long long, ushort is not
+// supported by atomicCAS (even though the doc says so)
+#if !defined(__CUDA_ARCH__)
+#else
+__device__ float atomicMax(float* address, float val)
+{
+    int* address_as_i = (int*)address;
+    int old = *address_as_i, assumed;
+    do
+    {
+        assumed = old;
+        old = atomicCAS(address_as_i,
+                        assumed,
+                        __float_as_int(fmaxf(val, __int_as_float(assumed))));
+    } while (assumed != old);
+    return __int_as_float(old);
+}
+
+__device__ double atomicMax(double* address, double val)
+{
+    unsigned long long int* address_as_ull = (unsigned long long int*)address;
+    unsigned long long int old = *address_as_ull, assumed;
+
+    do
+    {
+        assumed = old;
+        old = atomicCAS(
+            address_as_ull,
+            assumed,
+            __double_as_longlong(fmax(val, __longlong_as_double(assumed))));
+
+        // Note: uses integer comparison to avoid hang in case of NaN (since NaN
+        // != NaN)
+    } while (assumed != old);
+
+    return __longlong_as_double(old);
+}
 #endif
